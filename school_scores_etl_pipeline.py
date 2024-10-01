66b246a68849063e4c4aa8b4e1c3830233261a74
Ammar NUH <Ammarsamirabdelghany.a.2018@ogr.iu.edu.tr>
2024-10-01 22:42:40 +0300

    Update school_scores_etl_pipeline.py

--- school_scores_etl_pipeline.py
+++ school_scores_etl_pipeline.py
@@ -19,80 +19,11 @@ raw_schools_scores = {
     "writing_score": [None, None, 601.0, 387.0, 415.0]
 }
 
-normalized_schools_scores = []
+# extract the scores data
+raw_schools_scores = extract(raw_schools_scores)
 
-def extract(raw_data):
-	
-# Loop through each of the dictionary key-value pairs
-for school_id, school_info in raw_data.items():
-	normalized_schools_scores.append([
-    	school_id,
-    	school_info.get("street_address"),  # Pull the "street_address"
-    	school_info.get("city"),
-    	school_info.get("scores").get("math", 0),
-    	school_info.get("scores").get("reading", 0),
-    	school_info.get("scores").get("writing", 0),
-    ])
-
-	print(normalized_schools_scores)
-	
-	# Create a DataFrame from the normalized_schools_scores list
-	normalized_data = pd.DataFrame(normalized_schools_scores)
-	
-	# Set the column names
-	normalized_data.columns = ["school_id", "street_address", "city", "avg_score_math", "avg_score_reading", "avg_score_writing"]
-	
-	normalized_data = normalized_data.set_index("school_id")
-	print(normalized_data.head())
-	
-	logging.info('Data extracted succssfuly')
-	
-#---------
-
-# Define a function to find the street name
-def find_street_name(row):
-    return ' '.join(row['street_address'].split()[1:])
-
-
-# Define a function to transform and clean the data
-def transform(raw_data):
-    # Use .loc[] to only return the needed columns
-    raw_data = raw_data.loc[:, ["city", "math_score", "reading_score", "writing_score"]]
-        
-    # Add street name extraction
-   # raw_data["street_name"] = raw_data.apply(find_street_name, axis=1)
-    
-    # Fill NaN values with column mean for scores
-    raw_data.fillna(
-        value={
-            "math_score": raw_data["math_score"].mean(),
-            "reading_score": raw_data["reading_score"].mean(),
-            "writing_score": raw_data["writing_score"].mean()
-        },
-        inplace=True
-    )
-	
-    logging.info('Data transformed succssfuly')
-	
-    return raw_data
-
-#----------------------
-
-# Function to load cleaned data into PostgreSQL
-def load(clean_data, con_engine):
-    # Store the data in the school's database
-    clean_data.to_sql(
-        name="schools_scores",
-        con=con_engine,
-        if_exists="replace",  # Make sure to replace existing data
-        index=True,
-        index_label="school_id"
-    )
-
-# Example usage
-
-# Clean the testing scores
-grouped_schools_scores = transform(normalized_data)
+# Clean the scores data
+cleaned_schools_scores = transform(normalized_data)
 
 # Define your PostgreSQL connection URL
 db_url = 'postgresql://username:password@localhost:5432/your_database'
@@ -101,13 +32,13 @@ db_url = 'postgresql://username:password@localhost:5432/your_database'
 db_engine = create_engine(db_url)
 
 # Load the cleaned data into PostgreSQL
-load(grouped_schools_scores, db_engine)
+load(cleaned_schools_scores, db_engine)
 
-# Query the data from the scores_by_city table and validate
+# Query the data from the schools_scores table and validate
 to_validate = pd.read_sql("SELECT * FROM schools_scores", con=db_engine)
 print(to_validate.head())
 
 
 #checkpoint
 # Check that the DataFrames are equal
-print(to_validate.equals(grouped_schools_scores))
+print(to_validate.equals(cleaned_schools_scores))
