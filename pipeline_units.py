b55eba96a76c527b94446ad6eb4023b10e03cf63
Ammar NUH <Ammarsamirabdelghany.a.2018@ogr.iu.edu.tr>
2024-10-01 22:53:38 +0300

    Update pipeline_units.py

--- pipeline_units.py
+++ pipeline_units.py
@@ -4,7 +4,6 @@ import logging
 
 
 def extract(raw_data):
-
 	normalized_schools_scores = []
 	
 	# Loop through each of the dictionary key-value pairs
@@ -17,9 +16,7 @@ def extract(raw_data):
 	    	school_info.get("scores").get("reading", 0),
 	    	school_info.get("scores").get("writing", 0),
 	    ])
-	
-		print(normalized_schools_scores)
-		
+			
 		# Create a DataFrame from the normalized_schools_scores list
 		normalized_data = pd.DataFrame(normalized_schools_scores)
 		
@@ -31,12 +28,11 @@ def extract(raw_data):
 		
 		logging.info('Data extracted succssfuly')
 
-#----------------------
-  # Define a function to find the street name
+
+# Define a function to find the street name
 def find_street_name(row):
     return ' '.join(row['street_address'].split()[1:])
 
-#----------------------
 
 # Define a function to transform and clean the data
 def transform(raw_data):
@@ -60,7 +56,6 @@ def transform(raw_data):
 	
     return raw_data
 
-#----------------------
 
 # Function to load cleaned data into PostgreSQL
 def load(clean_data, con_engine):
