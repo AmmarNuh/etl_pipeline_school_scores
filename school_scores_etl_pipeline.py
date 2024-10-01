3c109f68f8396d54a7269067b5ae4db3f90df12c
Ammar NUH <Ammarsamirabdelghany.a.2018@ogr.iu.edu.tr>
2024-10-01 18:39:59 +0300

    Update school_scores_etl_pipeline.py

--- school_scores_etl_pipeline.py
+++ school_scores_etl_pipeline.py
@@ -2,8 +2,20 @@ import pandas as pd
 import json
 
 # Load the JSON data
-with open("nested_school_scores.json", "r") as file:
-    raw_testing_scores = json.load(file)
+raw_testing_scores = {
+    "school_id": ["02M260", "06M211", "01M539", "02M294", "02M308"],
+    "street_address": [
+        "425 West 33rd Street",
+        "650 Academy Street",
+        "111 Columbia Street",
+        "350 Grand Street",
+        "350 Grand Street"
+    ],
+    "city": ["Manhattan", "Manhattan", "Manhattan", "Manhattan", "Manhattan"],
+    "math_score": [None, None, 657.0, 395.0, 418.0],
+    "reading_score": [None, None, 601.0, 411.0, 428.0],
+    "writing_score": [None, None, 601.0, 387.0, 415.0]
+}
 
 normalized_testing_scores = []
 
