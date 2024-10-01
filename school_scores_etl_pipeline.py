23358a1cf490dd3d4aa11a89797557d07806eb93
Ammar NUH <Ammarsamirabdelghany.a.2018@ogr.iu.edu.tr>
2024-10-01 18:02:28 +0300

    Update adding transformation fun.py

--- school_scores_etl_pipeline.py
+++ school_scores_etl_pipeline.py
@@ -18,17 +18,29 @@ for school_id, school_info in raw_testing_scores.items():
         school_info.get("scores").get("writing", 0),
     ])
 
-# Print normalized testing scores
-print(normalized_testing_scores)
-
 # Create a DataFrame from the normalized_testing_scores list
 normalized_data = pd.DataFrame(normalized_testing_scores)
 
 # Set the column names
-normalized_data.columns = ["school_id", "street_address", "city", "avg_score_math", "avg_score_reading", "avg_score_writing"]
+normalized_data.columns = ["school_id", "street_address", "city", "math_score", "reading_score", "writing_score"]
 
 # Set school_id as the index
 normalized_data = normalized_data.set_index("school_id")
 
-# Print the first few rows of the normalized DataFrame
-print(normalized_data.head())
+# Define a function to clean the data
+def transform(raw_data):
+    raw_data.fillna(
+        value={
+            "math_score": raw_data["math_score"].mean(),
+            "reading_score": raw_data["reading_score"].mean(),
+            "writing_score": raw_data["writing_score"].mean()
+        },
+        inplace=True
+    )
+    return raw_data
+
+# Clean the testing scores
+clean_testing_scores = transform(normalized_data)
+
+# Print the first few rows of the cleaned DataFrame
+print(clean_testing_scores.head())
