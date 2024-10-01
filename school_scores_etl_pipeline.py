3cbdc55c344d2db0d7e0ca38e10fb55012ff8a7d
Ammar NUH <Ammarsamirabdelghany.a.2018@ogr.iu.edu.tr>
2024-10-01 22:52:31 +0300

    Update school_scores_etl_pipeline.py

--- school_scores_etl_pipeline.py
+++ school_scores_etl_pipeline.py
@@ -4,7 +4,6 @@ import logging
 from sqlalchemy import create_engine
 from school_scores_etl_pipeline import extract, transform, load
 
-# Load the JSON data
 raw_schools_scores = {
     "school_id": ["02M260", "06M211", "01M539", "02M294", "02M308"],
     "street_address": [
@@ -20,25 +19,34 @@ raw_schools_scores = {
     "writing_score": [None, None, 601.0, 387.0, 415.0]
 }
 
-# extract the scores data
-raw_schools_scores = extract(raw_schools_scores)
 
-# Clean the scores data
-cleaned_schools_scores = transform(normalized_data)
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-# Define your PostgreSQL connection URL
-db_url = 'postgresql://username:password@localhost:5432/your_database'
+try:
+    # extract the scores data
+    raw_schools_scores = extract(raw_schools_scores)
     
-# Create a database connection
-db_engine = create_engine(db_url)
+    # Clean the scores data
+    cleaned_schools_scores = transform(normalized_data)
+    
+    # Define your PostgreSQL connection URL
+    db_url = 'postgresql://username:password@localhost:5432/your_database'
+        
+    # Create a database connection
+    db_engine = create_engine(db_url)
+    
+    # Load the cleaned data into PostgreSQL
+    load(cleaned_schools_scores, db_engine)
 
-# Load the cleaned data into PostgreSQL
-load(cleaned_schools_scores, db_engine)
+	logging.info("Successfully extracted, transformed and loaded data.")  # Log a success message.
+    
+except Exception as e:
+		logging.error(f"Pipeline failed with error: {e}")  # Log failure message
+
 
 # Query the data from the schools_scores table and validate
 to_validate = pd.read_sql("SELECT * FROM schools_scores", con=db_engine)
 print(to_validate.head())
-
 
 #checkpoint
 # Check that the DataFrames are equal
